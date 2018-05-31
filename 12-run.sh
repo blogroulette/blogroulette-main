4c06be9dd3f2669168855977d4c11a52b6dd8961
Sascha Scherrer <dev@saschascherrer.de>
2018-05-31 17:36:59 +0200

    Add git status to run script.
    Signed-off-by: Sascha Scherrer <dev@saschascherrer.de>

--- 12-run.sh
+++ 12-run.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+echo "blogroulette is at $(git status --branch --short | head -1)"
+echo "frontend     is at $(cd frontend && git status --branch --short | head -1)"
+echo "backend      is at $(cd backend && git status --branch --short | head -1)"
+
 set -x
 
 wildfly-12.0.0.Final/bin/standalone.sh &
